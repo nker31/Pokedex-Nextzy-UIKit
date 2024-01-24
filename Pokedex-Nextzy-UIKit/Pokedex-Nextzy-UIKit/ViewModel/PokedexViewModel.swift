76c02409f1e50e12217a139908f9bd59fe8fe816
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-01-24 17:02:32 +0700

    Update PokedexViewModel.swift
    - added return type to fecthPokemonAPI

--- Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/ViewModel/PokedexViewModel.swift
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/ViewModel/PokedexViewModel.swift
@@ -10,25 +10,32 @@ import Alamofire
 
 class PokedexViewModel{
     var pokemons: [Pokemon]?
-    let pokemonURL = "https://raw.githubusercontent.com/wirunpong-j/PokedexAPIMock/master/pokemons.json"
     
-    func fecthPokemonAPI() async{
+    func fecthPokemonAPI() async -> [Pokemon]{
         
         // pokemon API endpoint
         let apiUrl = "https://raw.githubusercontent.com/wirunpong-j/PokedexAPIMock/master/pokemons.json"
         
         // make API call
-        AF.request(apiUrl).responseDecodable(of: [Pokemon].self){ response in
+        await AF.request(apiUrl).responseDecodable(of: [Pokemon].self){ response in
             switch response.result {
                 // if success
             case .success(let pokemonArray):
                 self.pokemons = pokemonArray
                 print("Debugger: Fetched pokemon data success")
+                
                 // if failed
-            case .failure(let error):
+            case .failure(_):
                 print("Debugger: Fetched pokemon data failed")
             }
         }
+        if let returnedPokemon = self.pokemons{
+            print("Debugger: returned pokemon \(returnedPokemon.count)")
+            return returnedPokemon
+        }
+        print("Debugger: returned nothing")
+        return []
+        
     }
 
 
