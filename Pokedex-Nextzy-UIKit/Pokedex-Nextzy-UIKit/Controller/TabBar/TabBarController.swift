6a5af2998cff15c1a2f58709d6148a41853b78fd
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-01-28 15:31:18 +0700

    added progress view to TabBar menu

--- Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/Controller/TabBar/TabBarController.swift
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/Controller/TabBar/TabBarController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class TabBarController: UITabBarController {
     
+    // MARK: - Varibles
     private let authViewModel: AuthViewModel
     private let pokedexViewModel: PokedexViewModel
     private let myPokemonViewModel: MyPokemonViewModel
     
+    // MARK: - Initializer
     init(authViewModel: AuthViewModel, pokedexViewModel: PokedexViewModel, myPokemonViewModel: MyPokemonViewModel) {
         self.authViewModel = authViewModel
         self.pokedexViewModel = pokedexViewModel
@@ -24,18 +26,35 @@ class TabBarController: UITabBarController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - UI Components
+    lazy var progressView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        view.isHidden = true
+        return view
+    }()
+    lazy var progresslabel: UILabel = {
+        let label = UILabel()
+        label.text = "Loading"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        return label
+    }()
+
+    
+    // MARK: - Life Cycle
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        progressView.isHidden = false
         Task{
             do{
                 await authViewModel.fetchUserData()
-                print("Debugger: currunt user \(String(describing: authViewModel.currentUser))")
                 if let currentUser = authViewModel.currentUser {
                     await myPokemonViewModel.fetchMyPokemon(userID: currentUser.id)
-                    print("Debugger: My Pokemon array \(myPokemonViewModel.myPokemonIDs)")
-                    print("Debugger: fetched my pokemon in tabmenu complete")
                 }
                 self.setupTab()
+                sleep(UInt32(2.0))
+                progressView.isHidden = true
             }
             
         }
@@ -46,21 +65,22 @@ class TabBarController: UITabBarController {
         super.viewDidLoad()
 
         self.setupTab()
-
-        // Set tab bar
-        self.tabBar.barTintColor = UIColor.systemBackground
-        self.tabBar.tintColor = UIColor.pinkPokemon
+        self.setupUI()
+        
 
         // Set navigation bar appearance
         UINavigationBar.appearance().barTintColor = UIColor.systemBackground
         UINavigationBar.appearance().tintColor = UIColor.pinkPokemon
 
     }
-
     
-    // MARK: - set up
-    
+    // MARK: - UI Setup
     private func setupTab(){
+        // Set tab bar
+        self.tabBar.barTintColor = UIColor.systemBackground
+        self.tabBar.tintColor = UIColor.pinkPokemon
+        
+        // assign viewController to tab menu
         let pokedex = self.createNav(with: "Pokedex", and: UIImage(systemName: "pawprint.fill"), vc: PokedexViewController(authViewModel: authViewModel,pokedexViewModel: pokedexViewModel, myPokedexViewModel: myPokemonViewModel))
         let myPokemon = self.createNav(with: "My Pokemon", and: UIImage(systemName: "heart.text.square"), vc: MyPokemonViewController(authViewModel: authViewModel, pokedexViewModel: pokedexViewModel, myPokemonViewModel: myPokemonViewModel))
         let profile = self.createNav(with: "Profile", and: UIImage(systemName: "person.fill"), vc: ProfileViewController(authViewModel: authViewModel))
@@ -76,5 +96,35 @@ class TabBarController: UITabBarController {
         nav.viewControllers.first?.navigationItem.title = title
         return nav
     }
+    
+    private func setupUI(){
+        
+        self.progressView.addSubview(progresslabel)
+        progresslabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(progressView)
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            progresslabel.centerXAnchor.constraint(equalTo: progressView.centerXAnchor),
+            progresslabel.centerYAnchor.constraint(equalTo: progressView.centerYAnchor),
+            
+            progressView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            progressView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            progressView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+
+            
+        ])
+        
+
+    }
+    
+    // MARK: - Selectors
+    
+    
+    
+    
+        
 
 }
