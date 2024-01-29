1cecac1645f0f189c75be986946da8fd0e5db615
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-01-29 14:49:23 +0700

    created LoginTextField component

---
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/View/Auth/LoginTextField.swift
@@ -0,0 +1,42 @@
+//
+//  LoginTextField.swift
+//  Pokedex-Nextzy-UIKit
+//
+//  Created by Nathat Kuanthanom on 29/1/2567 BE.
+//
+
+import UIKit
+
+class LoginTextField: UITextField {
+    
+    enum TextFieldType{
+        case email
+        case password
+    }
+
+    let type: TextFieldType
+    
+    init(textfieldType: TextFieldType) {
+        self.type = textfieldType
+        super.init(frame: .zero)
+        self.autocapitalizationType = .none
+        self.borderStyle = .none
+        self.placeholder = "Enter your email"
+        self.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.size.height))
+        self.leftViewMode = .always
+        
+        switch type{
+            
+        case .email:
+            self.placeholder = "Enter your email"
+        case .password:
+            self.placeholder = "Enter your password"
+            self.isSecureTextEntry = true
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
