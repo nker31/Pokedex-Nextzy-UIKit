624197ba924b3943e7925c7edc184d9c34566342
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-02-04 22:28:32 +0700

    refactor(DetailViewModel): add viewmodel method

--- Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/ViewModel/TabBar/DetailViewModel.swift
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/ViewModel/TabBar/DetailViewModel.swift
@@ -8,13 +8,24 @@
 import Foundation
 
 protocol DetailViewModelDelegate {
-    func toggleViewReload()
+    func toggleNavbarReload()
+    func toggleTableViewReload()
 }
 
 class DetailViewModel {
+    enum selectedMenu {
+        case about
+        case stat
+        case evolution
+    }
+    
     private let pokemonManager = PokemonManager.shared
     private let myPokemonManager = MyPokemonManager.shared
     private let authManager = AuthenticationManager.shared
+    var delegate: DetailViewModelDelegate?
+    
+    var isLiked = false
+    var isPresentMenu = selectedMenu.about
     var pokemon: Pokemon
     var filteredPokemon: [Pokemon] = []
     var myPokemonID: [String]
@@ -22,6 +33,14 @@ class DetailViewModel {
     init(pokemon: Pokemon) {
         self.pokemon = pokemon
         self.myPokemonID = myPokemonManager.myPokemonsID
+        self.filterEvolutionPokemon()
+    }
+    
+    func filterEvolutionPokemon() {
+        if let pokemons = pokemonManager.pokemons {
+            self.filteredPokemon = pokemons.filter { self.pokemon.evolutions.contains($0.id) }
+        }
+        print("Debugger: Filtered Pokemon = \(self.filteredPokemon)")
     }
     
     func tapFavorite(pokemonID: String) {
@@ -39,6 +58,27 @@ class DetailViewModel {
         
         Task {
             await myPokemonManager.addPokemonToFavList(pokemonID: self.myPokemonID, userID: userID)
-        }  
+        }
+        
+        checkFavorite()
+    }
+    
+    func checkFavorite() {
+        isLiked = myPokemonID.contains(pokemon.id)
+        delegate?.toggleNavbarReload()
+    }
+    
+    func tapChangeMenu(index: Int) {
+        switch index {
+        case 0:
+            isPresentMenu = selectedMenu.about
+        case 1:
+            isPresentMenu = selectedMenu.stat
+        case 2:
+            isPresentMenu = selectedMenu.evolution
+        default:
+            isPresentMenu = selectedMenu.about
+        }
+        delegate?.toggleTableViewReload()
     }
 }
