343a92310f72b1f3eda4f5cc27e1990951c79985
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-02-04 23:28:00 +0700

    create SearchViewModel

---
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/ViewModel/TabBar/Pokedex/SearchViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  SearchViewModel.swift
+//  Pokedex-Nextzy-UIKit
+//
+//  Created by Nathat Kuanthanom on 4/2/2567 BE.
+//
+
+import Foundation
+
+protocol SearchViewModelDelegate {
+    func toggleCollectionViewReload()
+    func toggleEmptyState(isHidden: Bool, searchText: String)
+}
+
+class SearchViewModel {
+    private let pokemonManager = PokemonManager.shared
+    var delegate: SearchViewModelDelegate?
+    var pokemons: [Pokemon] = []
+    var filteredPokemon: [Pokemon] = []
+    
+    init() {
+        self.setPokemon()
+    }
+    
+    func setPokemon() {
+        guard let pokemons = pokemonManager.pokemons else {
+            return
+        }
+        self.pokemons = pokemons
+    }
+    
+    func onSearchTextChange(searchText: String) {
+        if searchText.isEmpty {
+            self.filteredPokemon = []
+        } else {
+            let result = pokemons.filter { $0.name.starts(with: searchText) }
+            
+            if result.isEmpty {
+                delegate?.toggleEmptyState(isHidden: false, searchText: searchText)
+                self.filteredPokemon = []
+            } else {
+                delegate?.toggleEmptyState(isHidden: true, searchText: searchText)
+                self.filteredPokemon = result
+            }
+        }
+        delegate?.toggleCollectionViewReload()
+    }
+}
