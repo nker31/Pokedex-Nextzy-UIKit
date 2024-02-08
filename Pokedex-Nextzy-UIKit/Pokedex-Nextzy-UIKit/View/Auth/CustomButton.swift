46e456586833a71ec1eafd6113aca2936763f4ad
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-02-08 13:13:17 +0700

    refactor(CustomButton): change font to notoSansMedium

--- Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/View/Auth/CustomButton.swift
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/View/Auth/CustomButton.swift
@@ -12,7 +12,7 @@ class CustomButton: UIButton {
     init(title: String, cornerRadius: CGFloat = 0){
         super.init(frame: .zero)
         self.setTitle(title, for: .normal)
-        self.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
+        self.titleLabel?.font = .notoSansMedium(size: 18)
         self.setTitleColor(.white, for: .normal)
         self.backgroundColor = UIColor(red: 0.941, green: 0.388, blue: 0.396, alpha: 1)
         self.alpha = 1
