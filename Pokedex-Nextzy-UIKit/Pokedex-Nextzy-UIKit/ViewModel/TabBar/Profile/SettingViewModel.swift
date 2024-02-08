76bcaafda78699a0623c38d059abd0e05d758f1b
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-02-08 11:12:07 +0700

    create SettingViewModel

---
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/ViewModel/TabBar/Profile/SettingViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  SettingViewModel.swift
+//  Pokedex-Nextzy-UIKit
+//
+//  Created by Nathat Kuanthanom on 8/2/2567 BE.
+//
+
+import Foundation
+
+protocol SettingViewModelDelegate {
+    func toggleAlert(messege: String)
+    func toggleViewReload()
+}
+
+class SettingViewModel {
+    
+    var delegate: SettingViewModelDelegate?
+    
+    func switchLanguage(languageCode: String) {
+        UserDefaults.standard.set([languageCode], forKey: "AppleLanguages")
+        UserDefaults.standard.synchronize()
+        delegate?.toggleViewReload()
+        delegate?.toggleAlert(messege: "Please retart app for change language")
+    }
+    
+}
