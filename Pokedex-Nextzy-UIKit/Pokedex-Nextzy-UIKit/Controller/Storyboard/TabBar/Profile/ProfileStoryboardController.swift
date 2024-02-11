e359c323828eef1100c1622560d649e0d178d204
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-02-11 15:13:42 +0700

    refactor(ProfileStoryboardController): add table cell segue

--- Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/Controller/Storyboard/TabBar/Profile/ProfileStoryboardController.swift
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/Controller/Storyboard/TabBar/Profile/ProfileStoryboardController.swift
@@ -11,10 +11,14 @@ class ProfileStoryboardController: UIViewController {
 
     // MARK: - Varibles
     private let profileViewModel: ProfileViewModel = ProfileViewModel()
+    private let cellTitle: [String] = [String(localized: "edit_profile_title"),
+                                       String(localized: "terms_and_conditions_title"),
+                                       String(localized: "language_setting_title")]
     
     // MARK: - UI Components
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userFullnameLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Life Cycle
     override func viewWillAppear(_ animated: Bool) {
@@ -26,7 +30,8 @@ class ProfileStoryboardController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         profileViewModel.delegate = self
-
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -38,10 +43,42 @@ class ProfileStoryboardController: UIViewController {
     @IBAction func didTapSignOutButton(_ sender: Any) {
         profileViewModel.tapSignOut(signOutType: .storyboard)
     }
+    
+    func configureCell(cell: UITableViewCell, rowIndex: Int) {
+        cell.textLabel?.text = cellTitle[rowIndex]
+        cell.textLabel?.textColor = .pinkPokemon
+        
+    }
+}
+
+extension ProfileStoryboardController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        configureCell(cell: cell, rowIndex: indexPath.row)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+            case 0:
+                performSegue(withIdentifier: "PresentEditProfile", sender: nil)
+            case 1:
+                performSegue(withIdentifier: "PresentWebView", sender: nil)
+            case 2:
+                performSegue(withIdentifier: "PresentSetting", sender: nil)
+            default:
+                break
+            }
+    }
 }
 
 extension ProfileStoryboardController: ProfileViewModelDelegate {
     func segueToNextView() {
+        hidesBottomBarWhenPushed = true
         performSegue(withIdentifier: "SignOutSuccess", sender: Any.self)
     }
     
@@ -52,8 +89,7 @@ extension ProfileStoryboardController: ProfileViewModelDelegate {
     }
     
     func navigateToNextView() {
-        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
-        sceneDelegate?.presentLoginViewController()
+        
     }
 
 }
