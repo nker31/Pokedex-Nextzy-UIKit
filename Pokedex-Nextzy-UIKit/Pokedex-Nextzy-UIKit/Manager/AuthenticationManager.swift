79ebc47a164142fcddb49778606f1941ea087f0d
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-02-12 02:12:46 +0700

    refactor(AuthenticationManager): remove unused AuthenticationManager extension

--- Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/Manager/AuthenticationManager.swift
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/Manager/AuthenticationManager.swift
@@ -148,17 +148,3 @@ class AuthenticationManager {
     }
     
 }
-
-extension AuthenticationManager {
-    
-    func isValidEmail(_ email: String) -> Bool {
-        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
-        return emailPredicate.evaluate(with: email)
-    }
-
-    func isValidPassword(_ password: String) -> Bool {
-        return password.count >= 8
-    }
-
-}
