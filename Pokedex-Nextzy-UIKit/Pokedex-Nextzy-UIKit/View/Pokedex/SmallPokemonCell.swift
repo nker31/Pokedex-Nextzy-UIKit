352cbd28fa73105a2da885ccacf373ed6d2018a4
Nathat Kuanthanom <nkpeam51@gmail.com>
2024-02-13 10:14:11 +0700

    refactor(SmallPokemonCell): change image placeholder

--- Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/View/Pokedex/SmallPokemonCell.swift
+++ Pokedex-Nextzy-UIKit/Pokedex-Nextzy-UIKit/View/Pokedex/SmallPokemonCell.swift
@@ -20,35 +20,27 @@ class SmallPokemonCell: UICollectionViewCell {
     }()
     
     // MARK: - UI Setup
-    func configPokemonCell(pokemon: Pokemon){
+    func configPokemonCell(pokemon: Pokemon) {
         self.layer.cornerRadius = 10
         self.setColorBackgroundFromType(type: pokemon.types[0])
         self.setupCellUI()
-        self.pokemonImage.kf.setImage(with: pokemon.imageUrl, placeholder: UIImage(named: "pokeball-profile"))
+        self.pokemonImage.kf.setImage(with: pokemon.imageUrl, placeholder: UIImage(named: "pokeball"))
     }
     
-    func setupCellUI(){
+    func setupCellUI() {
         self.addSubview(pokemonImage)
         self.pokemonImage.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            
             self.pokemonImage.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             self.pokemonImage.centerYAnchor.constraint(equalTo: self.centerYAnchor),
             self.pokemonImage.widthAnchor.constraint(equalToConstant: (self.frame.width / 1.5)),
             self.pokemonImage.heightAnchor.constraint(equalToConstant: (self.frame.width / 1.5))
-        
         ])
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-
-        self.pokemonImage.image = nil
-        self.backgroundColor = .white
-        for subview in self.subviews {
-            subview.removeFromSuperview()
-        }
     }
 }
 
